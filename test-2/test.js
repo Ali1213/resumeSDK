c3495d851829bbddb5c184588d3f324975599cd1
yu ali <yuali@yudeMac-mini.local>
2018-03-30 16:28:32 +0800

    second commit

--- test-2/test.js
+++
@@ -1,9 +0,0 @@
-const resumeParse = require('../index');
-const util = require('util');
-const fs = require('fs');
-const path = require('path');
-const log = console.log.bind(console);
-resumeParse(path.join(__dirname,'resume_Zheng Qian.pdf')).catch(e=>log(e)).then((result)=>{
-  console.log(result);
-  return util.promisify(fs.writeFile)(path.join(__dirname,'resume_Zheng Qian.json'),JSON.stringify(result))
-});
